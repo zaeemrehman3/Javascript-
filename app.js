7ed67f68f8184efd897686314d89510c24e6f613
Zaeem Rehman <zaeemrehman39@gmail.com>
2024-02-17 11:53:12 +0500

    alerts

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hello js");
+// Exercise 1
+
+
+// Answer 1
+alert("Welcome to my Website,Thanks for Visiting.");
+
+// Answer 2
+alert("Error! Please Enter a Valid Password.");
+
+// Answer 3
+alert('Welcome to JS Land... \n Happy Coding!');
+
