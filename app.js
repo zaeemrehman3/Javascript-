2597f955c458dfd93891d505760dbb1ceea86295
Zaeem Rehman <zaeemrehman39@gmail.com>
2024-02-22 13:13:48 +0500

    solved

--- app.js
+++ app.js
@@ -1,12 +1,127 @@
-// Exercise 1
+// Chapter 1
+// Q1:
+
+// alert("Welcome to my Website,Thanks for Visiting.");
+
+// Q2:
+
+// alert("Error! Please Enter a Valid Password.");
+
+// Q3:
+
+// alert('Welcome to JS Land... \n Happy Coding!');
+
+// Q4:
+ 
+// alert('Welcome to JS land');
+// alert('Happy coding! \n Prevent this page from additional dialogs');
+
+// Q5: 
+// Simple you create popup by usning console when you inspect the page, simply type alert('') and type message between the brackets 
 
 
-// Answer 1
-alert("Welcome to my Website,Thanks for Visiting.");
 
-// Answer 2
-alert("Error! Please Enter a Valid Password.");
+// Chapter 2
+// Q1:
 
-// Answer 3
-alert('Welcome to JS Land... \n Happy Coding!');
+// var userName = " ";
+
+// Q2:
+
+// var userName = 'Zaeem Rehman';
+
+// Q3:
+
+// var message = 'Hello World';
+// alert(message)
+
+// Q4:
+
+// var firstName = 'John Doe';
+// alert(firstName)
+
+// var age = ( 15 + "years old");
+// alert(age)
+
+// var message = 'Certified Mobile Application Development';
+// alert(message)
+
+// Q5:
+
+// alert('Pizza \n Pizz \n Piz \n Pi \n P');
+
+// Q6:
+
+// var email = 'My email address is zaeemrehman39@gmail.com';
+// alert(email)
+
+// Q7:
+
+// var book = 'i am tyring to learn from the book. A smarter \n way to learn javascript';
+// alert(book)
+
+// Q8:
+
+// document.body.innerHTML = 'Yah! i can write content through javascript';
+
+
+
+// Chapter 3
+// Q1:
+
+// var age = 18;
+// alert( ' I am ' + age + ' Years Old ' );
+
+// Q2:
+
+// var number = 14;
+// alert( ' You have visited this site ' + number + ' times ' );
+
+// Q3:
+
+// var birthYear = 2005;
+// document.body.innerHTML = 'My birth year is 2005 <br\> data type of my declared variable is number';
+
+
+
+// Chapter 4
+// Q1:
+
+// var a, b, c
+
+// Q2:
+
+// Legal Variable:
+
+// var myVariable
+// var _myVariable
+// var $myVariable
+// var myVariable12
+// var my_variable
+// var va12riable
+
+// Illegal Variable:
+
+// var 12myVariable
+// var my Variable 
+// var my-Variable
+// var !myVariable
+// var @myVariable
+
+// Q3:
+
+// Rules for Variables:-
+// 1-Varible can only start with letters not number
+// 2-No special characters use in the stating of variable except two characters i.e "$" and "_"
+// 3-Javascript is case sensitive programming language if you dont follow the syntax rules accuratley you will get Error!
+
+
+
+
+
+
+
+
+
+
 
