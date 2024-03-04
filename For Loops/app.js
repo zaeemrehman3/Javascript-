4a6014dfd150182e30b86a2b0b640116266a409b
Zaeem Rehman <zaeemrehman39@gmail.com>
2024-03-04 22:58:28 +0500

    change

---
+++ For Loops/app.js
@@ -0,0 +1,28 @@
+// Q1:
+
+// let i = 0;
+// for(i=0; i<5; i++){
+//     console.log('Hello World');
+// }
+
+// const userInput = +prompt('Enter the Table Number');
+// const TableInput = +prompt('Enter the length');
+
+// for (let i = 1; i <= TableInput; i++) {
+
+//     console.log(`${userInput} * ${i} = ${i*userInput}`);
+// }
+
+// const Fruits = ['Apple','Banana','mango','orange','Strawberry'];
+
+// const firstDiv = document.querySelector('.single-item');
+// const secondDiv = document.querySelector('.index');
+
+// for(let i=o; i <= Fruits.length; i++){
+//     firstDiv.innerHTML += '<div>${Fruits[i]}</div>'
+//     secondDiv.innerHTML += '<div>${Fruits[i]}</div>'
+//     console.log(Fruits);
+// }
+
+const arrayA = ['cake','apple pie','cookie','chips','patties'];
+const Search = prompt('Search Items');
